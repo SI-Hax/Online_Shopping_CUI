c8a21e0d69b9942463c8da9b530d45949e4faab6
Roxy <>
2021-03-21 22:00:50 +1300

    Product Class

---
+++ src/online_shopping_cui/Product.java
@@ -0,0 +1,90 @@
+/*
+* This class contains the Product Class which encapsulates the following methods:
+*Data:
+*- Produce Name (String), Product Description (String)
+*- Product ID (int), Price (double), stock (Integer)
+*- Category (Category enum)
+*
+* Methods:
+*- Empty Constructor
+*- Five parameter Constructor
+*- Getters and Setters
+*
+* @author  Miguel Emmara - 18022146
+* @author  Amos Foong - xxx
+* @author  Roxy Dao - 1073633
+* @version 1.0 
+* @since   15/03/2021
+ */
+package online_shopping_cui;
+
+public class Product {
+
+    private String productName;
+    private String description;
+    private int productID;
+    private double price;
+    private Category category;
+    private Integer stock;
+
+    public Product() {
+
+    }
+
+    public Product(String productName, int productID, double price, Category category, String description, Integer stock) {
+        this.productName = productName;
+        this.productID = productID;
+        this.price = price;
+        this.category = category;
+        this.description = description;
+        this.stock = stock;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+}
