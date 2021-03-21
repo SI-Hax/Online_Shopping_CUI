3ed96e57eb72eb5d171899d614dcb1de1bd88f58
Roxy <>
2021-03-21 21:57:09 +1300

    State Enums

---
+++ src/online_shopping_cui/State.java
@@ -0,0 +1,15 @@
+/*
+* This class contains the State Enumeration which maintains constant variables
+* for the user account state
+* 
+* @author  Miguel Emmara - 18022146
+* @author  Amos Foong - xxx
+* @author  Roxy Dao - 1073633
+* @version 1.0 
+* @since   15/03/2021
+ */
+package online_shopping_cui;
+
+public enum State {
+    NEW, ACTIVE, SUSPENDED, BANNED
+}
