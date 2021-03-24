544ac17d54c82c36296f61578f7ae0ceb42132f6
Miguel <miguelemmara123@gmail.com>
2021-03-25 12:46:29 +1300

    Test Miguel

--- src/online_shopping_cui/Main.java
+++ src/online_shopping_cui/Main.java
@@ -24,5 +24,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Pushing...");
         System.out.println("Amos was here.");
+        System.out.println("Test with Intellij");
     }
 }
