65b2ac7e9824bbf9084ef7a6e1b1c348b9e8b8ac
Miguel Emmara <m.miguelemmara@gmail.com>
2021-03-30 23:09:37 +1300

    Add Customer and Administrator Class

---
+++ src/online_shopping_cui/Administrator.java
@@ -0,0 +1,35 @@
+package online_shopping_cui;
+
+public class Administrator {
+    private String adminName;
+    private String adminEmail;
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    public String getAdminEmail() {
+        return adminEmail;
+    }
+
+    public void setAdminEmail(String adminEmail) {
+        this.adminEmail = adminEmail;
+    }
+
+    public void addProduct() {
+
+    }
+
+    public void removeProduct(int productID) {
+
+    }
+    public void editProduct() {
+
+    }
+
+
+}
---
+++ src/online_shopping_cui/Customer.java
@@ -0,0 +1,114 @@
+package online_shopping_cui;
+
+/**
+ * This class handles the customer actions.
+ * It inherit User Class.
+ *
+ *
+ * @author Miguel Emmara - 18022146
+ * @author Amos Foong - 18044418
+ * @author Roxy Dao - 1073633
+ * @version 1.0
+ * @since 30/03/2021
+ **/
+
+public class Customer extends User{
+    private String customerName;
+    private String address;
+    private int phone;
+    private String email;
+    private int cardNumber;
+    private int cardExpDate;
+    private int cvv;
+    private String cardType;
+    private String cardName;
+
+    /**
+     * 1-parameter constructor for User class. Subclasses of User must
+     * call super() and pass in the loginID to initialise their account.
+     *
+     * <p>Newly created accounts have their state set to Active.</p>
+     *
+     * @param loginID : user's login id.
+     **/
+    public Customer(String loginID) {
+        super(loginID);
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public int getCardExpDate() {
+        return cardExpDate;
+    }
+
+    public void setCardExpDate(int cardExpDate) {
+        this.cardExpDate = cardExpDate;
+    }
+
+    public int getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(int cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    @Override
+    public boolean setPassword(String password) {
+        return false;
+    }
+}
