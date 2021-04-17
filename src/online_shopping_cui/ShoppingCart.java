dd627e698814589f7bc98db7c6f2d5022cfc2f66
Amos Foong <80317637+djorange77@users.noreply.github.com>
2021-04-17 21:09:43 +1200

    -Added a method to generate invoice.

--- src/online_shopping_cui/ShoppingCart.java
+++ src/online_shopping_cui/ShoppingCart.java
@@ -36,13 +36,37 @@ public class ShoppingCart {
         this.quantity.remove(index);
     }
     
+    public void clearCart() {
+        this.products.clear();
+        this.quantity.clear();
+        this.grandTotal = 0D;
+    }
+    
     public String cartList(){
         String cartList = "";
         
         for(int i = 0; i < this.products.size(); i++) {
-            cartList += "Product: " + this.products.get(i).getProductName() + " Qty: " + this.quantity.get(i) + " Price: " + this.products.get(i).getPrice(); 
+            cartList += "Product: " + this.products.get(i).getProductName() + " Qty: " + this.quantity.get(i) + " Price: " + this.products.get(i).getPrice() +"\n"; 
         }
         
         return cartList;
     }
+    
+    public String generateInvoice(User currentUser){
+        String invoice = "";
+        
+        invoice += "-----------------Invoice----------------\n";
+        invoice += "Bill To: " + ((Customer)currentUser).getName() + "\n";
+        invoice += "Billing Address: " + ((Customer)currentUser).getAddress() + "\n";
+        invoice += "----------------------------------------\n";
+        invoice += String.format("%20s%10s%7s\n", "Product", "Quantity", "Price");
+        
+        for(int i = 0; i < this.products.size(); i++) {
+             invoice += String.format("%20s %8d  $%7.2f\n", this.products.get(i).getProductName(), this.quantity.get(i), (this.products.get(i).getPrice() * this.quantity.get(i)));
+        }
+        invoice += "----------------------------------------\n";
+        invoice += String.format("%32s%7.2f\n", "Grand Total: $", this.getGrandTotal());
+        invoice += "----------------------------------------\n";
+        return invoice; 
+    }       
 }
