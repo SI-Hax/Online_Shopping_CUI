56d6efcff66fe283671f9f551c66e3d25811606b
Amos Foong <80317637+djorange77@users.noreply.github.com>
2021-04-17 16:45:51 +1200

    -Restructured the UserInterface class

--- src/online_shopping_cui/ShoppingCart.java
+++ src/online_shopping_cui/ShoppingCart.java
@@ -6,26 +6,32 @@ import java.util.ArrayList;
  * @author Amos Foong <18044418>
  */
 public class ShoppingCart {
-    protected static int CART_ID = 0;
     private ArrayList<Product> products;
     private ArrayList<Integer> quantity;
     private double grandTotal;
+    private User user;
 
-    public ShoppingCart() {
-        ShoppingCart.CART_ID++;
+    public ShoppingCart(User user) {
         this.products = new ArrayList<Product>();
         this.quantity = new ArrayList<Integer>();
         this.grandTotal = 0.0;
+        this.user = user;
     }
-
+    
+    public double getGrandTotal() {
+        return this.grandTotal;
+    }
+    
     public void addItemToCart(Product product, int amount) {
         this.products.add(product);
         this.quantity.add(amount);
+        product.setStock(product.getStock() - amount); // Decrements product stock.
         this.grandTotal += (product.getPrice() * amount);
     }
 
     public void removeItemFromCart(int index) {
         this.grandTotal -= (this.products.get(index).getPrice() * this.quantity.get(index));
+        this.products.get(index).setStock(this.products.get(index).getStock() + this.quantity.get(index)); // Increments product's stock.
         this.products.remove(index);
         this.quantity.remove(index);
     }
--- src/online_shopping_cui/UserInterface.java
+++ src/online_shopping_cui/UserInterface.java
@@ -1,24 +1,37 @@
 package online_shopping_cui;
 
 import java.io.*;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class UserInterface {
     public static final String FILE_PATH = "./resources/customer database.csv";
+    public static Scanner scanner;
+    public HashMap<String, User> users;
+    public User currentUser;
+    public boolean mainMenuLoop, accountCreationSuccess, loginSuccess;
+    
+    public UserInterface()
+    {
+        this.scanner = new Scanner(System.in);
+        this.users = UserFileIO.importUserData();
+        this.mainMenuLoop = true;
+        this.accountCreationSuccess = false;
+        this.loginSuccess = false;
+        
+    }
 
-    private void mainMenu() {
+    private void mainMenu() 
+    {
         System.out.println("\n\t1. Login");
         System.out.println("\t2. Create Account");
-        // TODO add Guest Options
         System.out.println("\t3. Exit");
     }
 
-    public void menuSelections() {
-        Scanner scanner = new Scanner(System.in);
+    public void menuSelections() 
+    {
         boolean stop = true;
         boolean stop2 = true;
-        boolean stop3 = true;
+
 
         do {
             try {
@@ -28,38 +41,10 @@ public class UserInterface {
                 scanner.nextLine();
                 switch (uAnswer) {
                     // Choose To Login As Administrator Or Customer
-                    case 1:
+                    case 1: // User selects login in level 1 menu...
                         do {
-                            try {
-                                System.out.println("\n\t1. Login As Customer");
-                                System.out.println("\t2. Login As Administrator");
-                                System.out.print("\nPlease Choose Your Option: ");
-                                uAnswer = scanner.nextInt();
-                                scanner.nextLine();
-
-                                switch (uAnswer) {
-                                    case 1:
-                                        customerLogin(scanner);
-                                        stop3 = false;
-                                        break;
-                                    case 2:
-                                        adminLogin(scanner);
-                                        stop3 = false;
-                                        break;
-                                    default:
-                                        throw new IndexOutOfBoundsException();
-                                }
-                            } catch (IndexOutOfBoundsException e) {
-                                System.err.println("Please Enter The Correct Options, 1 - 2");
-                                System.err.flush();
-                            } catch (InputMismatchException | IllegalArgumentException e) {
-                                System.err.println("Please Enter The Correct Options, 1 - 2");
-                                System.err.flush();
-                                scanner.nextLine();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        } while (stop3);
+                            loginSuccess = loginSelection();
+                        } while (!loginSuccess);
                         break;
                     // Choose To Register As Administrator Or Customer
                     case 2:
@@ -73,10 +58,10 @@ public class UserInterface {
 
                                 switch (uAnswer) {
                                     case 1:
-                                        stop2 = createCustomerAccount(scanner);
+                                        accountCreationSuccess = createCustomerAccount();
                                         break;
                                     case 2:
-                                        stop2 = createAdministratorAccount(scanner);
+                                        accountCreationSuccess = createAdministratorAccount();
                                         break;
                                     default:
                                         throw new IndexOutOfBoundsException();
@@ -89,11 +74,11 @@ public class UserInterface {
                                 System.err.flush();
                                 scanner.nextLine();
                             }
-                        } while (stop2);
+                        } while (!accountCreationSuccess);
                         break;
                     case 3:
                         System.out.println("\nGood Bye");
-                        stop = false;
+                        mainMenuLoop = false;
                         break;
                     default:
                         throw new IndexOutOfBoundsException();
@@ -106,23 +91,142 @@ public class UserInterface {
                 System.err.flush();
                 scanner.nextLine();
             }
-        } while (stop);
+        } while (mainMenuLoop);
+    }
+    
+    public boolean loginSelection()
+    {
+        int userSelection = -1;
+        
+        System.out.println("\n\t1. Login As Customer");
+        System.out.println("\t2. Login As Administrator");
+        System.out.println("\t3. Continue as Guest");
+        System.out.println("\t");
+        System.out.print("\nPlease Choose Your Option: ");
+        
+        try 
+        {
+            userSelection = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (userSelection) {
+                case 1:
+                    customerLogin();
+                    return false;
+                case 2:
+                    adminLogin();
+                    return false;
+                case 3:
+                    // TODO: Something...
+                    return false;
+                default:
+                    throw new IndexOutOfBoundsException();
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Please Enter The Correct Options, 1 - 3");
+            System.err.flush();
+        } catch (InputMismatchException | IllegalArgumentException e) {
+            System.err.println("Please Enter The Correct Options, 1 - 3");
+            System.err.flush();
+            scanner.nextLine();
+        } 
+        
+        return true;
+    }
+    
+    private boolean customerLogin() 
+    {
+        boolean promptLogin = true;
+        do 
+        {
+            System.out.print("\n\tLogin ID: ");
+            String loginID = scanner.nextLine();
+
+            if(loginID.equalsIgnoreCase("b")) { // If user wishes to go back...
+                return false; // Exits method and returns false to caller to go back up a level for menu.
+            } else if (this.users.containsKey(loginID)) { // If user-specified login is in the database...
+                boolean promptPassword = true;
+
+                do {
+                    // Prompt user for their password.
+                    System.out.print("\tPassword: ");
+                    String password = scanner.nextLine();
+
+                    if (this.users.get(loginID).getPassword().equals(password)) { // If user-specified password match
+                        this.currentUser = this.users.get(loginID);
+                        System.out.println("\nLogin Successful, Welcome Back " + ((Customer)currentUser).getName()); // Welcomes user.
+                        promptLogin = false;
+                        promptPassword = false;
+                        return true; // Exits method and returns true to caller.
+                    } else if (password.equalsIgnoreCase("b")) { // If user wishes to go back up a level...
+                        return false; // Exits method and returns false to caller to go back up a level for menu.
+                    } else { // Otherwise....
+                        System.out.println("Passwords do not match!");
+                        promptPassword = true; 
+                    }
+                } while(promptPassword);
+            } else { // If user-specified login ID is non-existent...
+                System.out.println("\nSorry, " + loginID + " is not a registered user!");
+                promptLogin = true;
+            }
+        } while(promptLogin);
+        
+        return false;
     }
 
-    private boolean createCustomerAccount(Scanner scanner) {
+    private boolean adminLogin() {
+        boolean promptLogin = true;
+        do 
+        {
+            System.out.print("\n\tLogin ID: ");
+            String loginID = scanner.nextLine();
+
+            if(loginID.equalsIgnoreCase("b")) { // If user wishes to go back...
+                return false; // Exits method and returns false to caller to go back up a level for menu.
+            } else if (this.users.containsKey(loginID)) { // If user-specified login is in the database...
+                boolean promptPassword = true;
+
+                do {
+                    // Prompt user for their password.
+                    System.out.print("\tPassword: ");
+                    String password = scanner.nextLine();
+
+                    if (this.users.get(loginID).getPassword().equals(password)) { // If user-specified password match
+                        this.currentUser = this.users.get(loginID);
+                        System.out.println("\nLogin Successful, Welcome Back " + ((Administrator)currentUser).getAdminName()); // Welcomes user.
+                        promptLogin = false;
+                        promptPassword = false;
+                        return true; // Exits method and returns true to caller.
+                    } else if (password.equalsIgnoreCase("b")) { // If user wishes to go back up a level...
+                        return false; // Exits method and returns false to caller to go back up a level for menu.
+                    } else { // Otherwise....
+                        System.out.println("Passwords do not match!");
+                        promptPassword = true; 
+                    }
+                } while(promptPassword);
+            } else { // If user-specified login ID is non-existent...
+                System.out.println("\nSorry, " + loginID + " is not a registered user!");
+                promptLogin = true;
+            }
+        } while(promptLogin);
+        
+        return false;
+    }
+
+    public boolean createCustomerAccount() {
         // TODO data validation in each prompt
         System.out.print("\nLogin ID: ");
         String loginID = scanner.nextLine();
 
-        while (loginID.isEmpty() || loginID.contains(" ")) {
-            System.out.println("Please enter a valid name!");
-            System.out.print("What Is Your User Name: ");
+        // While user-defined login ID is empty or exists within collection...
+        while (loginID.isEmpty() || users.containsKey(loginID)) {
+            // Promp user for another login id.
+            System.out.print("Please choose another login ID:");
             loginID = scanner.nextLine();
         }
 
         System.out.print("Password: ");
         String password = scanner.nextLine();
-        String encryptedPassword = Utilities.encrypt(password);
 
         System.out.print("Full Name: ");
         String name = scanner.nextLine();
@@ -133,7 +237,7 @@ public class UserInterface {
         System.out.print("Email: ");
         String email = scanner.nextLine();
 
-        System.out.print("address: ");
+        System.out.print("Address: ");
         String address = scanner.nextLine();
 
         System.out.print("Card Number: ");
@@ -142,110 +246,34 @@ public class UserInterface {
         System.out.print("Card Holder Name: ");
         String cardHolder = scanner.nextLine();
 
-        Customer customer = new Customer(loginID, encryptedPassword, name, phone, email, address, cardNumber, cardHolder);
-        customer.writeCSV();
+        this.users.put(loginID, new Customer(loginID, password, name, phone, email, address, cardNumber, cardHolder));
 
         return false;
     }
 
-    private boolean createAdministratorAccount(Scanner scanner) {
-        // TODO create admin database
-        System.out.print("\nName Of The Company: ");
-        String companyName = scanner.nextLine();
-
+    private boolean createAdministratorAccount() 
+    {
         System.out.print("\nLogin ID: ");
         String loginID = scanner.nextLine();
 
-        while (loginID.isEmpty() || loginID.contains(" ")) {
-            System.out.println("Please enter a valid name!");
-            System.out.print("What Is Your User Name: ");
+        while (loginID.isEmpty() || users.containsKey(loginID)) {
+            // Promp user for another login id.
+            System.out.print("Please choose another login ID:");
             loginID = scanner.nextLine();
         }
 
         System.out.print("Password: ");
         String password = scanner.nextLine();
-        String encryptedPassword = Utilities.encrypt(password);
 
-        System.out.print("Full Name: ");
+        System.out.print("Name: ");
         String name = scanner.nextLine();
-
-        System.out.print("Phone Number: ");
-        String phone = scanner.nextLine();
 
         System.out.print("Email: ");
         String email = scanner.nextLine();
 
-        System.out.print("address: ");
-        String address = scanner.nextLine();
-
-
-        System.out.print("Card Number: ");
-        String cardNumber = scanner.nextLine();
-
-        System.out.print("Card Holder Name: ");
-        String cardHolder = scanner.nextLine();
-
+        this.users.put(loginID, new Administrator(loginID, password, name, email));
+        
         return false;
-    }
-
-    private void customerLogin(Scanner scanner) throws IOException {
-        boolean stop = true;
-        do {
-            try {
-                System.out.print("\n\tLogin ID: ");
-                String loginID = scanner.nextLine();
-
-                while (loginID.isEmpty() || loginID.contains(" ")) {
-                    System.out.println("Please enter a valid name!");
-                    System.out.print("What Is Your User Name: ");
-                    loginID = scanner.nextLine();
-                }
-
-                System.out.print("\tPassword: ");
-                String decryptedPassword = scanner.nextLine();
-                String encryptedPassword = Utilities.encrypt(decryptedPassword);
-
-                Customer customer = new Customer(loginID, encryptedPassword);
-
-                File csvFile = new File(FILE_PATH);
-                if (csvFile.isFile()) {
-                    if (customer.checkLoginID(loginID)) {
-                        if (customer.checkPassword(encryptedPassword)) {
-                            System.out.println("\nLogin Successful, Welcome Back " + customer.getName());
-                            stop = false;
-                        } else {
-                            System.out.println("\nWrong Password");
-                        }
-                    } else {
-                        System.out.println("\nSorry, " + loginID + " is not a registered user!");
-                    }
-                } else {
-                    System.err.println("\nFile did not exist!");
-                    break;
-                }
-            } catch (InputMismatchException | IllegalArgumentException e) {
-                System.err.println("Please Enter The Correct Options, 1 - 3");
-                System.err.flush();
-                scanner.nextLine();
-            }
-        } while (stop);
-
-    }
-
-    private void adminLogin(Scanner scanner) {
-        System.out.print("\n\tLogin ID: ");
-        String loginID = scanner.nextLine();
-
-        System.out.print("\tPassword: ");
-        String password = scanner.nextLine();
-
-        System.out.print("\tAdmin Name: ");
-        String adminName = scanner.nextLine();
-
-        System.out.print("\tAdmin Email: ");
-        String adminEmail = scanner.nextLine();
-
-        Administrator administrator = new Administrator(loginID, password, adminName, adminEmail);
     }
 
 }
