c3fabfbfcb1a3212a1e7535facf786fa5e1c1887
Amos Foong <80317637+djorange77@users.noreply.github.com>
2021-04-17 19:48:22 +1200

    -CartList method added

--- src/online_shopping_cui/ShoppingCart.java
+++ src/online_shopping_cui/ShoppingCart.java
@@ -22,17 +22,27 @@ public class ShoppingCart {
         return this.grandTotal;
     }
     
-    public void addItemToCart(Product product, int amount) {
+    public void addToCart(Product product, int amount) {
         this.products.add(product);
         this.quantity.add(amount);
         product.setStock(product.getStock() - amount); // Decrements product stock.
         this.grandTotal += (product.getPrice() * amount);
     }
 
-    public void removeItemFromCart(int index) {
+    public void removeFromCart(int index) {
         this.grandTotal -= (this.products.get(index).getPrice() * this.quantity.get(index));
         this.products.get(index).setStock(this.products.get(index).getStock() + this.quantity.get(index)); // Increments product's stock.
         this.products.remove(index);
         this.quantity.remove(index);
+    }
+    
+    public String cartList(){
+        String cartList = "";
+        
+        for(int i = 0; i < this.products.size(); i++) {
+            cartList += "Product: " + this.products.get(i).getProductName() + " Qty: " + this.quantity.get(i) + " Price: " + this.products.get(i).getPrice(); 
+        }
+        
+        return cartList;
     }
 }
