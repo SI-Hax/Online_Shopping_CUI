4ac3a5f2dc14dc73f3f83c1e335b228a93bcb2b0
roxeh <61820074+roxy-codes@users.noreply.github.com>
2021-04-20 11:16:02 +1200

    Realignment

--- src/com/online/shopping_cui/model/Customer.java
+++ src/com/online/shopping_cui/model/Customer.java
@@ -3,25 +3,25 @@ package com.online.shopping_cui.model;
 import com.online.shopping_cui.utilities.Utilities;
 
 /**
- * This class holds information about a Customer.
- * It is an extension of User class. Behaviours include
- * getters and setters that uses externally-sourced
+ * This class holds information about a Customer. It is an extension of User
+ * class. Behaviours include getters and setters that uses externally-sourced
  * validation methods (from Utilities class).
  *
- * <p>Attributes:</p>
+ * <p>
+ * Attributes:</p>
  * <ul>
- *  <li>Customer's Name</li>
- *  <li>Phone Number</li>
- *  <li>E-mail Address</li>
- *  <li>Shipping/Billing Address</li>
- *  <li>Card Number</li>
- *  <li>Card Holder</li>
+ * <li>Customer's Name</li>
+ * <li>Phone Number</li>
+ * <li>E-mail Address</li>
+ * <li>Shipping/Billing Address</li>
+ * <li>Card Number</li>
+ * <li>Card Holder</li>
  * </ul>
  * Behaviours:
  * <ul>
- *  <li>2-Parameter Constructor</li>
- *  <li>8-Parameter Constructor</li>
- *  <li>To String Method</li>
+ * <li>2-Parameter Constructor</li>
+ * <li>8-Parameter Constructor</li>
+ * <li>To String Method</li>
  * </ul>
  *
  * @author Miguel Emmara - 18022146
@@ -29,8 +29,10 @@ import com.online.shopping_cui.utilities.Utilities;
  * @author Roxy Dao - 1073633
  * @version 1.03
  * @since 30/03/2021
- **/
+ *
+ */
 public class Customer extends User {
+
     private String name;
     private String phone;
     private String email;
@@ -38,14 +40,14 @@ public class Customer extends User {
     private String cardNumber;
     private String cardHolder;
 
-    //Constructors-------------------------------------------------------------
     /**
-     * 2-parameter constructor for Customer class. This is the most basic
-     * constructor. User must have at least a loginID and password to sign up.
+     * 2-parameter constructor for Customer class. User must have at least a 
+     * loginID and password to sign up.
      *
-     * @param loginID  : User's login identifier.
+     * @param loginID : User's login identifier.
      * @param password : User-defined password.
-     **/
+     *
+     */
     public Customer(String loginID, String password) {
         super(loginID);
         this.setPassword(password);
@@ -58,22 +60,23 @@ public class Customer extends User {
     }
 
     /**
-     * 8-parameter constructor for Customer class. This constructor
-     * accounts for essential and non-essential details of a shopper.
-     * Calls relevant set methods to perform checks (for data validity),
-     * if data passes checks, it is then stored into the Object's attribute.
+     * 8-parameter constructor for Customer class. This constructor accounts for
+     * essential and non-essential details of a shopper. Calls relevant set
+     * methods to perform checks (for data validity), if data passes checks, it
+     * is then stored into the Object's attribute.
      *
-     * @param loginID    : User's login identifier.
-     * @param password   : User-defined password.
-     * @param name       : Customer's name.
-     * @param phone      : Contact phone number.
-     * @param email      : User's email address.
-     * @param address    : Shipping/billing address.
+     * @param loginID : User's login identifier.
+     * @param password : User-defined password.
+     * @param name : Customer's name.
+     * @param phone : Contact phone number.
+     * @param email : User's email address.
+     * @param address : Shipping/billing address.
      * @param cardNumber : Card number.
      * @param cardHolder : Card holder's name.
-     **/
+     *
+     */
     public Customer(String loginID, String password, String name, String phone,
-                    String email, String address, String cardNumber, String cardHolder) {
+            String email, String address, String cardNumber, String cardHolder) {
         super(loginID);
         this.setPassword(password);
         this.setName(name);
@@ -84,7 +87,6 @@ public class Customer extends User {
         this.setCardHolder(cardHolder);
     }
 
-    //Getter and Setters----------------------------------------------------------------
     public String getName() {
         return name;
     }
@@ -106,7 +108,7 @@ public class Customer extends User {
     }
 
     public void setEmail(String email) throws IllegalArgumentException {
-        if(email.equalsIgnoreCase("UNKNOWN")) { // Checks if passed in data is "UNKNOWN"...
+        if (email.equalsIgnoreCase("UNKNOWN")) { // Checks if passed in data is "UNKNOWN"...
             this.email = "UNKNOWN";
         } else if (!(email.isEmpty() || email.equals(" "))) { // Checks if passed in data is not empty...
             if (Utilities.emailIsValid(email)) { // If passed in email passes check...
@@ -132,7 +134,7 @@ public class Customer extends User {
     }
 
     public void setCardNumber(String cardNumber) throws IllegalArgumentException {
-        if(cardNumber.equalsIgnoreCase("UNKNOWN")) { // Checks if passed in data is "UNKNOWN"...
+        if (cardNumber.equalsIgnoreCase("UNKNOWN")) { // Checks if passed in data is "UNKNOWN"...
             this.cardNumber = "UNKNOWN";
         } else if (!(cardNumber.isEmpty() || cardNumber.equals(" "))) { // Checks if passed in data is not empty...
             if (Utilities.cardIsValid(cardNumber.trim())) { // If passed in card number passes check...
@@ -154,21 +156,24 @@ public class Customer extends User {
     }
 
     /**
-     * Overridden method from superclass (User). Users that are Customers
-     * are required to have a password length of at least 8 characters.
+     * Overridden method from superclass (User). Users that are Customers are
+     * required to have a password length of at least 8 characters.
      *
-     * <p>In addition, the password must also meet general password requirements
+     * <p>
+     * In addition, the password must also meet general password requirements
      * (At least: 1 Uppercase, 1 Lowercase, 1 Number, and 1 Symbol).</p>
-     * 
-     * <p>Examples:</p>
+     *
+     * <p>
+     * Examples:</p>
      * <ul>
-     *  <li>Invalid Password : GeeksForGeeks</li>
-     *  <li>Valid Password   : Geek$ForGeeks7</li>
+     * <li>Invalid Password : GeeksForGeeks</li>
+     * <li>Valid Password : Geek$ForGeeks7</li>
      * </ul>
      *
      * @param password : User defined password.
      * @throws IllegalArgumentException
-     **/
+     *
+     */
     @Override
     public void setPassword(String password) throws IllegalArgumentException {
         // Check if password length is at least 8 characters and if its secure...
@@ -180,15 +185,15 @@ public class Customer extends User {
     }
 
     /**
-     * To String method to serialise object. Stringifies Object's
-     * attributes, which are separated by commas.
+     * To String method to serialise object. String files Object's attributes,
+     * which are separated by commas.
      *
-     * @return A String containing the User's type, Customer's login
-     *           credentials and details, all separated by commas.
-     **/
+     * @return A String containing the User's type, Customer's login credentials
+     * and details, all separated by commas.
+     *
+     */
     @Override
-    public String toString()
-    {
+    public String toString() {
         String comma = ",";
         String details = "";
 
@@ -203,6 +208,6 @@ public class Customer extends User {
         details += this.getCardHolder();
         details += "\n";
 
-        return details;  
+        return details;
     }
 }
