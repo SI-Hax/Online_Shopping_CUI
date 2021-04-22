2bbced947e381e0d478452c5c72b3835dbeb0afe
Amos Foong <80317637+djorange77@users.noreply.github.com>
2021-04-23 04:50:52 +1200

    -Rectified the stack overflow error caused from recursive constructor calls.

--- src/com/online/shopping_cui/driver/CreateAccount.java
+++ src/com/online/shopping_cui/driver/CreateAccount.java
@@ -5,16 +5,11 @@
  */
 package com.online.shopping_cui.driver;
 
-import com.online.shopping_cui.enumerations.Category;
 import com.online.shopping_cui.model.Customer;
-import com.online.shopping_cui.model.ProductList;
 import com.online.shopping_cui.model.User;
 import com.online.shopping_cui.model.Administrator;
-import com.online.shopping_cui.model.ShoppingCart;
-import com.online.shopping_cui.model.Product;
-import com.online.shopping_cui.utilities.ProductFileIO;
 import com.online.shopping_cui.utilities.Utilities;
-import com.online.shopping_cui.utilities.UserFileIO;
+
 import java.util.*;
 
 /**
@@ -24,7 +19,7 @@ import java.util.*;
  * @author Miguel Emmara - 18022146
  * @author Amos Foong - 18044418
  * @author Roxy Dao - 1073633
- * @version 1.01
+ * @version 1.03
  * @since 18/04/2021
  *
  */
@@ -33,12 +28,12 @@ public class CreateAccount {
     static String REQUIRED = "\n*Required Fields (\"b\" to go back or press \"enter\" to skip non-essential fields)";
     static String PASSWORDSTRENGTH = "Password is not strong enough (Requirements: 1 Upper, 1 Lower, 1 Number, 1 Symbol, and ";
 
-    protected static Scanner scanner;
+    protected Scanner scanner;
     protected HashMap<String, User> users;
 
-    public CreateAccount() {
-        this.scanner = new Scanner(System.in);
-        this.users = UserFileIO.importUserData();
+    public CreateAccount(Scanner scanner, HashMap<String, User> users) {
+        this.scanner = scanner;
+        this.users = users;
     }
 
     /**
@@ -51,24 +46,24 @@ public class CreateAccount {
         boolean saved = false;
 
         System.out.println(REQUIRED);
-        System.out.print("\tLogin ID*: ");
-        loginID = scanner.nextLine();
-
-        if (loginID.equalsIgnoreCase("b")) { // If user wishes to go back...
-            return false;
-        }
-
-        while (loginID.isEmpty() || users.containsKey(loginID)) {// While user-defined login ID is empty or exists within collection...
-            // Promp user for another login id.
-            System.err.print("Please choose another login ID: ");
+        do{
+            System.out.print("\tLogin ID*: ");
             loginID = scanner.nextLine();
-        }
+            
+            if (loginID.equalsIgnoreCase("b")) { // If user wishes to go back...
+                return false; // Go back up a level.
+            } else if(loginID.trim().isEmpty() || users.containsKey(loginID)){ // While user-defined login ID is empty or exists within collection...
+                System.err.println("Please choose another login ID."); // Prompt user for another login id.
+            }
+        }  while (loginID.trim().isEmpty() || users.containsKey(loginID)); // While user-defined login ID is empty or exists within collection...
 
         do {
             System.out.print("\tPassword*: ");
             password = scanner.nextLine();
 
-            if (!Utilities.passIsSecure(password, 8)) { // If user-defined password is not secure enough...
+            if (password.equalsIgnoreCase("b")) { // If user wishes to go back...
+                return false; // Go back up a level.
+            } else if (!Utilities.passIsSecure(password, 8)) { // If user-defined password is not secure enough...
                 System.err.println(PASSWORDSTRENGTH + "8+ Characters): "); // Output guide/prompt.
             }
         } while (!Utilities.passIsSecure(password, 8)); // While user-defined password is not secure.
@@ -118,28 +113,29 @@ public class CreateAccount {
      * @return T/F whether account was created.
      */
     protected boolean createAdministratorAccount() {
-        String loginID, password, name, email;
+        String loginID, password;
         boolean saved = false;
 
         System.out.println(REQUIRED);
-        System.out.print("\tLogin ID*: ");
-        loginID = scanner.nextLine();
-
-        if (loginID.equalsIgnoreCase("b")) { // If user wishes to go back...
-            return false;
-        }
-
-        while (loginID.isEmpty() || users.containsKey(loginID)) {// While user-defined login ID is empty or exists within collection...
-            // Promp user for another login id.
-            System.err.print("Please choose another login ID: ");
+        do{
+            System.out.print("\tLogin ID*: ");
             loginID = scanner.nextLine();
-        }
+            
+            if (loginID.equalsIgnoreCase("b")) { // If user wishes to go back...
+                return false; // Go back up a level.
+            } else if(loginID.trim().isEmpty() || users.containsKey(loginID)){ // If user-defined login ID is empty or exists within collection...
+                System.err.println("Please choose another login ID."); // Prompt user for another login id.
+            }
+        }  while (loginID.trim().isEmpty() || users.containsKey(loginID)); // While user-defined login ID is empty or exists within collection...
+
 
         do {
             System.out.print("\tPassword*: ");
             password = scanner.nextLine();
 
-            if (!Utilities.passIsSecure(password, 14)) { // If user-defined password is not secure enough...
+            if (password.equalsIgnoreCase("b")) { // If user wishes to go back...
+                return false; // Go back up a level.
+            } else if (!Utilities.passIsSecure(password, 14)) { // If user-defined password is not secure enough...
                 System.err.println(PASSWORDSTRENGTH + "14+ Characters): "); // Output guide/prompt.
             }
         } while (!Utilities.passIsSecure(password, 14)); // While user-defined password is not secure
