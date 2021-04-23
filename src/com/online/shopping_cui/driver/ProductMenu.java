1e97041548eb0b1bf70a5f6b33f1698a82f2f26f
Miguel Emmara <m.miguelemmara@gmail.com>
2021-04-23 20:57:47 +1200

    Delete License Header

--- src/com/online/shopping_cui/driver/ProductMenu.java
+++ src/com/online/shopping_cui/driver/ProductMenu.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.online.shopping_cui.driver;
 
 import com.online.shopping_cui.enumerations.Category;
